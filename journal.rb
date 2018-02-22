ac63e1a4c3f30417d1a716c4be91ad9b9608f97e
Katarzyna Mildner <itsyehudit@gmail.com>
2018-02-22 22:33:54 +0100

    Fix: undefined variables errors fixed.

--- journal.rb
+++ journal.rb
@@ -1,5 +1,37 @@
-entries = ["This entry is empty, choose other one"]
-availableEntries = entries.length - 1
+$entries = ["This entry is empty, choose other one"]
+$entry = nil
+
+def entriesMenu
+  puts " = Choose an option =
+
+  a Read entry
+  b Edit entry
+  c Delete entry
+  d <<< Return to Main Menu\n\n"
+
+  menu2 = gets.chomp
+  case menu2
+  when "a"
+    puts "Choose one of #{$entries.length-1}"
+    read = gets.to_i
+    puts $entries[read]
+  when "b"
+    puts "Choose one of #{$entries.length-1}"
+    read = gets.to_i
+    $entries.delete_at(read)
+    puts "Please edit your entry:"
+    $entry = gets.to_s
+    $entries.insert(read, $entry)
+  when "c"
+    puts "Choose one of #{$entries.length-1}"
+    read = gets.to_i
+    $entries.delete_at(read)
+  when "d"
+    mainMenu
+  else
+    puts "Something went wrong, try again."
+  end
+end
 
 def mainMenu
   puts "= Welcome to your Journal! =
@@ -14,8 +46,8 @@ def mainMenu
 
   case menu
   when 1
-    entry = gets.to_s
-    entries.push(entry)
+    $entry = gets.to_s
+    $entries.push($entry)
   when 2
     entriesMenu
   when 3
@@ -26,35 +58,3 @@ def mainMenu
 end
 
 mainMenu
-
-def entriesMenu
-  puts " = Choose an option =
-
-  a Read entry
-  b Edit entry
-  c Delete entry
-  d <<< Return to Main Menu\n\n"
-
-  menu2 = gets.chomp
-  case menu2
-  when "a"
-    puts "Choose one of #{availableEntries}"
-    read = gets.to_i
-    puts entries[read]
-  when "b"
-    puts "Choose one of #{availableEntries}"
-    read = gets.to_i
-    entries.delete_at(read)
-    puts "Please edit your entry:"
-    entry = gets.to_s
-    entries.insert(read, entry)
-  when "c"
-    puts "Choose one of #{availableEntries}"
-    read = gets.to_i
-    entries.delete_at(read)
-  when "d"
-    mainMenu
-  else
-    puts "Something went wrong, try again."
-  end
-end
