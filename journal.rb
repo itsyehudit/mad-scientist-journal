eaa45d55ddab7cc4b58b7c865489f9f490b2fd05
Katarzyna Mildner <itsyehudit@gmail.com>
2018-02-22 07:58:19 +0100

    Redesign: main Menu; defined main Menu method.

--- journal.rb
+++ journal.rb
@@ -1,24 +1,24 @@
-puts "= Welcome to your Journal! =
+def mainMenu
+  puts "= Welcome to your Journal! =
 
-1. Add entry -UNAVAILABLE-
-2. Edit entry -UNAVAILABLE-
-3. Read entry
-4. Quit
+  1 Add entry -UNAVAILABLE-
+  2 View entries -UNAVAILABLE-
+  3 Quit
 
+  Choose an option\n\n"
 
-Choose an option\n\n"
+  menu = gets.to_i
 
-menu = gets.to_i
+  case menu
+  when 1
 
-case menu
-when 3
-  puts "Entry 00.00.00"
-  puts "Lorem ipsum dolor sit amet,"
-  puts "gloriatur comprehensam qui ad,"
-  puts "maiorum convenire disputationi in qui,"
-  puts "id ius dico integre."
-when 4
-  puts "Bye!"
-else
-  puts "Something went wrong, try again."
+  when 2
+    entriesMenu
+  when 3
+    puts "Bye!"
+  else
+    puts "Something went wrong, try again."
+  end
 end
+
+mainMenu
