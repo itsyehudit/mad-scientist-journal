33005efb3cae5cadcce1750f42a447806ac288c2
Katarzyna Mildner <itsyehudit@gmail.com>
2018-02-23 00:00:25 +0100

    Redesign: added messages clarifying what is currently happening and which entry is being read, modified or deleted; added name to the goodbye message.

--- journal.rb
+++ journal.rb
@@ -17,30 +17,33 @@ def entriesMenu
   menu2 = gets.chomp
   case menu2
   when "a"
-    puts "Choose one of #{$entries.length-1}"
+    puts "Choose one of #{$entries.length-1}."
     read = gets.to_i
+    puts "Your #{read} entry:"
     puts $entries[read]
     puts "Continue reading? Y/N"
     continue = gets.chomp
     if continue === "y" || continue === "Y" || continue === "YES" || continue === "yes"
-      puts "Choose one of #{$entries.length-1}"
+      puts "Choose one of #{$entries.length-1}."
       read = gets.to_i
       puts $entries[read]
     else
       entriesMenu
     end
   when "b"
-    puts "Choose one of #{$entries.length-1}"
+    puts "Choose one of #{$entries.length-1}."
     read = gets.to_i
     $entries.delete_at(read)
-    puts "Please edit your entry:"
+    puts "Please edit your #{read} entry:"
     $entry = gets.to_s
     $entries.insert(read, $entry)
+    puts "Your #{read} entry was successfuly edited."
     entriesMenu
   when "c"
-    puts "Choose one of #{$entries.length-1}"
+    puts "Choose one of #{$entries.length-1}."
     read = gets.to_i
     $entries.delete_at(read)
+    puts "Your #{read} entry was successfuly deleted."
     entriesMenu
   when "d"
     mainMenu
@@ -64,13 +67,15 @@ def mainMenu
 
   case menu
   when 1
+    puts "Please add your entry below:"
     $entry = gets.to_s
     $entries.push($entry)
+    puts "Your entry was successfuly added."
     mainMenu
   when 2
     entriesMenu
   when 3
-    puts "Bye!"
+    puts "Bye, #{$name}!"
   else
     puts "Something went wrong, try again."
     mainMenu
