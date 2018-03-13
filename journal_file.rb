28968a4c439d202937be20cfc2a9ef8284d65272
Katarzyna Mildner <itsyehudit@gmail.com>
2018-03-13 19:27:06 +0100

    Refactor: added module Editable to journal files

--- journal_file.rb
+++ journal_file.rb
@@ -1,4 +1,5 @@
 require "yaml"
+require_relative "modules"
 
 class JournalFile
   def initialize
@@ -12,26 +13,7 @@ class JournalFile
     end
   end
 
-  def add_entry(new_entry)
-    @entries.push(new_entry)
-  end
-
-  def available_entries_count
-    @entries.length
-  end
-
-  def entry(entry_number)
-    @entries[entry_number]
-  end
-
-  def edit_entry(entry_number, replacement_entry)
-    @entries.delete_at(entry_number)
-    @entries.insert(entry_number, replacement_entry)
-  end
-
-  def delete_entry(entry_number)
-    @entries.delete_at(entry_number)
-  end
+  include Editable
 
   def quit
     File.open("journal_data.yaml", "w") do |j|
--- journal_memory.rb
+++ journal_memory.rb
@@ -1,28 +1,11 @@
+require_relative "modules"
+
 class JournalMemory
   def initialize
     @entries = []
   end
 
-  def add_entry(new_entry)
-    @entries.push(new_entry)
-  end
-
-  def available_entries_count
-    @entries.length
-  end
-
-  def entry(entry_number)
-    @entries[entry_number]
-  end
-
-  def edit_entry(entry_number, replacement_entry)
-    @entries.delete_at(entry_number)
-    @entries.insert(entry_number, replacement_entry)
-  end
-
-  def delete_entry(entry_number)
-    @entries.delete_at(entry_number)
-  end
+  include Editable
 
   def quit
   end
