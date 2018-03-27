990f458616dc041af855131bd003cb8c0c80d861
Katarzyna Mildner <itsyehudit@gmail.com>
2018-03-27 12:46:55 +0200

    Test refactor: memory_journal.rb - adding entry

--- test_memory_journal.rb
+++ test_memory_journal.rb
@@ -2,16 +2,20 @@ require 'rspec'
 require_relative 'memory_journal'
 
 describe MemoryJournal do
-  let(:journal) {MemoryJournal.new}
-  let(:entries) {journal.entries}
 
-  describe 'adding entry' do
+  describe '#add_entry' do
     it 'adds the entry to the entries pool' do
-      journal.add_entry('contentcontentcontent', 'monday', 'date-00-00-00')
-      expect(entries[0].content).to include 'contentcontentcontent'
-      expect(entries[0].title).to include 'monday'
-      expect(entries[0].date).to include 'date-00-00-00'
+      journal = MemoryJournal.new
+      title = 'monday'
+      content = 'contentcontentcontent'
+      date = '2018-03-14 03-14-00'
+
+      journal.add_entry(content, title, date)
+
+      first_entry = journal.entries[0]
+      expect(first_entry.title).to eq('monday')
+      expect(first_entry.content).to eq('contentcontentcontent')
+      expect(first_entry.date).to eq('2018-03-14 03-14-00')
     end
   end
-
 end
