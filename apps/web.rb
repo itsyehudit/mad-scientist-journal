282b90e95b025dea23e7419f3feb22920c3ba074
Katarzyna Mildner <itsyehudit@gmail.com>
2018-04-23 23:17:26 +0200

    New feature: added index controller and view

--- apps/web.rb
+++ apps/web.rb
@@ -1,8 +1,10 @@
 require 'sinatra'
+require_relative '../lib/file_journal'
 
 set :views, settings.root + '/web/views'
 set :public_folder, settings.root + '/web/stylesheets'
 
 get '/' do
+  @title = "Main Menu"
   haml :index
 end
