cfd284023c53345cdb706663621ffb20339aa6bb
Katarzyna Mildner <33177843+itsyehudit@users.noreply.github.com>
2018-04-29 02:24:04 +0900

    Implementation of web UI (#5)
    * Added HAML to the project
    
    * Added Web UI beginning
    
    * Added Shotgun to the project
    
    * Added separate layout file to the web UI
    
    * New feature: added index controller and view
    
    * New feature: added add entry controller and view
    
    * New feature: added entries menu controller and view
    
    * New feature: added view controller and view
    
    * New feature: added entry controller and view
    
    * New feature: added entry edition controller and view
    
    * New feature: added entry deletion controller and view
    
    * Fix: CLRF issues, changed style.css, index.haml and layout.haml to LF
    
    * Redesign: changed style and fixed font issue
    
    * Refactor: removed obsolete element: entries menu controller and view
    
    * New feature: added entry edition, deletion and going back to main menu in entries list view
    
    * Refactor: added images as local files
    
    * Refactor: moved shotgun to the development group and added rspec to the development and test group in Gemfile
    
    * Refactor: added proper spaces to journal assignment
    
    * Refactor: removed confirmation screen for entry deletion - delete entry immediately with each delete button
    
    * Refactor: moved bg images to a separate folder, edited style.css, layout.haml and web.rb files accordingly to maintain proper functionality
    
    * New feature: date saved as UTC, read as local time

--- Gemfile
+++ Gemfile
@@ -1,4 +1,13 @@
 source 'https://rubygems.org'
 
-gem 'rspec'
 gem 'sinatra'
+gem 'haml'
+
+group :development do
+  gem 'shotgun'
+  gem 'rspec'
+end
+
+group :test do
+  gem 'rspec'
+end
---
+++ apps/web.rb
@@ -0,0 +1,72 @@
+require 'sinatra'
+require_relative '../lib/file_journal'
+
+set :views, settings.root + '/web/views'
+set :public_folder, settings.root + '/web'
+
+get '/' do
+  @title = "Main Menu"
+  haml :index
+end
+
+get '/add' do
+  @title = "Add Entry"
+  haml :add_entry
+end
+
+post '/add' do
+  journal = FileJournal.new
+  title = params[:title]
+  content = params[:content]
+  local_date = Time.now
+  date = local_date.utc
+  journal.add_entry(content, title, date)
+  journal.quit
+
+  redirect "/view"
+end
+
+get '/view' do
+  journal = FileJournal.new
+  @entries = journal.entries
+  @title = "Entries"
+  haml :view
+end
+
+get '/view/:index' do
+  journal = FileJournal.new
+  @id = params[:index].to_i
+  @entry = journal.entries[@id]
+  @title = "Entry no. #{@id}"
+  haml :entry
+end
+
+get '/edit/:index' do
+  journal = FileJournal.new
+  @id = params[:index].to_i
+  @entry = journal.entries[@id]
+  @title = "Edit entry no. #{@id}"
+  haml :entry_edition
+end
+
+post '/edit/:index' do
+  journal = FileJournal.new
+  id = params[:index].to_i
+  entry = journal.entries[id]
+  title = params[:title]
+  content = params[:content]
+  date = entry.date
+  journal.edit_entry(id, content, title, date)
+  journal.quit
+
+  redirect "/view/#{id}"
+end
+
+post '/delete/:index' do
+  journal = FileJournal.new
+  id = params[:index].to_i
+  journal.delete_entry(id)
+  journal.quit
+
+  redirect "/view"
+end
